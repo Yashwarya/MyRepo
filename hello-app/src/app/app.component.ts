bf1f8bd70c723016c8e9237fc5f71e074ab87bbd
Yashwarya <yashwaryagupta24@gmail.com>
2020-09-14 13:14:27 +0530

    angular

---
+++ hello-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content">
+      <h1>
+        Welcome to {{title}}!
+      
+    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'hello-app';
+}
